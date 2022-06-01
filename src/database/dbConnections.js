63fd434491dfc5e3df92d01e8cad510b9260b45d
Alex <alextufielamigo@gmail.com>
2022-05-31 20:04:23 -0400

    direccion

--- src/database/dbConnections.js
+++ src/database/dbConnections.js
@@ -5,13 +5,13 @@ const database = () => {
     if (!pg) {
         console.log('base de datos NO CONECTADA')
     } else {
-        const pool = new pg.Pool({
-            host: 'localhost',
-            user: 'root',
-            password: 'root',
-            port: '5432',
-            database: 'prueba'
-        })
+        // const pool = new pg.Pool({
+        //     host: 'localhost',
+        //     user: 'root',
+        //     password: 'root',
+        //     port: '5432',
+        //     database: 'prueba'
+        // })
         // const pool = new pg.Pool({
         //     host: 'ec2-3-223-213-207.compute-1.amazonaws.com',
         //     user: 'ysfqdhjvugerje',
@@ -19,12 +19,12 @@ const database = () => {
         //     port: '5432',
         //     database: 'dj3g747tins5p'
         // })
-        // const pool = new pg.Pool({
-        //     connectionString:'postgres://ysfqdhjvugerje:116ff7aedfa19323a8e06608a92d97bb83eb5c4c0b1e76368a7bff5841fea1fa@ec2-3-223-213-207.compute-1.amazonaws.com:5432/dj3g747tins5p',
-        //     ssl:{
-        //         rejectUnauthorized:false
-        //     }
-        // })
+        const pool = new pg.Pool({
+            connectionString:'postgres://ysfqdhjvugerje:116ff7aedfa19323a8e06608a92d97bb83eb5c4c0b1e76368a7bff5841fea1fa@ec2-3-223-213-207.compute-1.amazonaws.com:5432/dj3g747tins5p',
+            ssl:{
+                rejectUnauthorized:false
+            }
+        })
         return pool
     }
 }
